611baa7686a57a0f7266848014b45bd0655b358e
Masrafi404 <mdmasrafi902@gmail.com>
2025-05-04 06:34:41 +0600

    right triangle pattern

---
+++ right_riangle.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int n, star = 1;
+    scanf("%d", &n);
+
+    int space = n - 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+
+        for (int a = space; a >= 1; a--)
+        {
+            printf(" ");
+        }
+        space--;
+
+
+        for (int j = 1; j <= star; j++)
+        {
+            printf("*");
+        }
+        star ++;
+        printf("\n");
+        
+    }
+
+    return 0;
+}
